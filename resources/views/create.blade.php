38e1d86aabad4ead718753660e4b945314a13409
kbprabowo <kbprabowo@outlook.com>
2023-03-29 21:16:08 +0800

    valdation error code on create new name

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -13,6 +13,15 @@
                             </div>
                         @endif
                         <h1>Add New Animal</h1>
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
                         <form action="/store" method="POST">
                             @csrf
                             <div class="mb-3">
